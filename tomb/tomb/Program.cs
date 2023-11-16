9d96a09acdfb741d155b66228e19a49b18eb8670
emojlyi <124292690+emojlyi@users.noreply.github.com>
2023-11-16 10:26:15 +0100

    Add files via upload

---
+++ tomb/tomb/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] emberek = new int[10];
+            int osszeg = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Kérem a(z) {i + 1} ember jegyét.");
+                emberek[i] = int.Parse( Console.ReadLine() );
+                osszeg += emberek[i];
+            }
+            double atlag = (double)osszeg / 10;
+            int darab = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (emberek[i] > atlag)
+                    darab++;
+
+            }
+            Console.WriteLine($"ennyien érték el az átlagnál jobb eredményt  {darab}");
+            Console.WriteLine($"Az átlaguk: {atlag}");
+            Console.ReadKey();
+        }
+    }
+}
