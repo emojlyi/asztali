998dd03a95b9273a585fdf6bd07d798cf8ecf739
emojlyi <124292690+emojlyi@users.noreply.github.com>
2023-11-16 10:30:00 +0100

    Add files via upload

---
+++ kivonas szorzas osztas/kivonas szorzas osztas/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kivonas_szorzas_osztas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ez a program egy számológép!");
+            int a;
+            Console.Write("Add meg az első számot");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Add meg a második számot");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Milyen műveletet akarsz elvégezni?(+,-,*,/):");
+            string szamolas = Console.ReadLine();
+
+            switch (szamolas)
+            {
+                case "+":
+                    a = szam1 + szam2;
+                    Console.WriteLine("Összeadás:" + a);
+                    string valasz = Console.ReadLine();
+                    if (szam1 == szam2)
+                    {
+
+                    }
+                    break;
+                case "-":
+                    a = szam1 - szam2;
+                    Console.WriteLine("Kivonás:" + a);
+                    break;
+                case "*":
+                    a = szam1 * szam2;
+                    Console.WriteLine("Szorzás:" + a);
+                    break;
+                case "/":
+                    a = szam1 / szam2;
+                    Console.WriteLine("Osztás:" + a);
+                    break;
+                default:
+                    Console.WriteLine("Rossz dolgot adtál meg!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
